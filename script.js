cc55bfaa3bf39b93b26b440e2637edf0575087df
EnzoLavieri <devenzolavieri@gmail.com>
2025-01-14 17:17:18 -0300

    logica adiconando apenas uma tarefa

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-// novaTarefa = prompt("escreva sua tarfa");
-// console.log(novaTarefa);
+tipoTarefa = prompt("Oque voce deseja fazer? (Adicionar | Remover)");
+switch (tipoTarefa) {
+  case "Adicionar":
+    novaTarefa = prompt("Escreva a tarefa que deseja fazer");
+    console.log(novaTarefa);
+    break;
+  case "Remover":
+    console.log("logica de remocao");
+    break;
+  default:
+    console.log("Opcao invalida, escreva `Adicionar` ou `Remover`");
+}
