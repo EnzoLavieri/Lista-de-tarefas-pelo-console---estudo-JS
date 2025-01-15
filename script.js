3776a021251cb1659c7606b615a9ce25297d84a9
EnzoLavieri <devenzolavieri@gmail.com>
2025-01-15 12:40:19 -0300

    new logic add, add function and task array

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-tipoTarefa = prompt("Oque voce deseja fazer? (Adicionar | Remover)");
-switch (tipoTarefa) {
-  case "Adicionar":
-    novaTarefa = prompt("Escreva a tarefa que deseja fazer");
-    console.log(novaTarefa);
-    break;
-  case "Remover":
-    console.log("logica de remocao");
-    break;
-  default:
-    console.log("Opcao invalida, escreva `Adicionar` ou `Remover`");
+// Criacao da variável tarefas, que é um array vazio. Esse array vai armazenar as tarefas que você adicionar.
+let listaDeTarefas = [];
+
+//A função adicionarTarefa recebe um parâmetro tarefa, que será uma string com a descrição da tarefa.
+//A função usa o método push() para adicionar a tarefa no final do array tarefas.
+//Em seguida, é exibida uma mensagem no console confirmando que a tarefa foi adicionada.
+function adiconarTarefa(tarefa) {
+  listaDeTarefas.push(tarefa);
+  console.log(`Tarefa "${tarefa} adicionada a lista.`);
 }
+
+alert("Bem-vindo(a) a lista de tarefas!");
+alert("Para adiconar uma nova tarefa, escreva `Adicionar`");
+alert("Para Remover uma tarefa, escreva `Remover`");
+alert("Para sair, escreva `Sair`");
+tipoTarefa = prompt("Oque voce deseja fazer? (Adicionar | Remover | Sair)");
